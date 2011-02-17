a16a1da416e1eb37ef3b265d326e62ea5d6c2e50
Dusty <git@dusty.name>
2011-02-16 21:39:53 -0300

    before_validate callback

--- lib/mongo_odm/document/callbacks.rb
+++ lib/mongo_odm/document/callbacks.rb
@@ -14,6 +14,7 @@ module MongoODM
 
         define_model_callbacks :initialize, :only => :after
         define_model_callbacks :save, :destroy
+        define_model_callbacks :validate, :only => :before
       end
       
       module InstanceMethods
