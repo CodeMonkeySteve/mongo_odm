ad0b4e5fe1a7ee0ea891a13c75a2b45119afd20b
Steve Sloan <steve@finagle.org>
2011-03-24 11:47:51 -0700

    Added #referenced_in for associations.

--- lib/mongo_odm/document/referable.rb
+++ lib/mongo_odm/document/referable.rb
@@ -7,6 +7,28 @@ module MongoODM
     module Referable
       extend ActiveSupport::Concern
 
+      module ClassMethods
+        def referenced_in(model, field = nil, opts = {})
+          model = model.to_s.camelize.constantize
+
+          n = self.name.underscore
+          field_name = field ||
+            (model.fields.include?(f = n.singularize.to_sym) && f) ||
+            (model.fields.include?(f = n.pluralize.to_sym) && f)
+          raise "Missing field" unless field_name
+          raise(UnknownFieldError, field_name, model) unless model.fields.include?(field_name)
+
+          method_name = opts[:as] || model.to_s.underscore.pluralize
+          raise "Field \"#{method_name}\" already defined in #{self.name}" if method_defined?(method_name.to_sym)
+
+          self.class_eval <<-EOT
+            def #{method_name}
+              #{model}.where('#{field_name}' => self.to_dbref )
+            end
+          EOT
+        end
+      end
+
       module InstanceMethods
         Reference = MongoODM::Reference
 
