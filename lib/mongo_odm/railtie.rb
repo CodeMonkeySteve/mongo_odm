b15a7ce0daf011b940e53eebe9278d47f804e80f
Steve Sloan <steve@finagle.org>
2011-06-06 21:00:01 -0700

    Minor fix for Rails 3.1.

--- lib/mongo_odm/railtie.rb
+++ lib/mongo_odm/railtie.rb
@@ -1,7 +1,8 @@
 module Rails #:nodoc:
   module MongoODM #:nodoc:
     class Railtie < Rails::Railtie #:nodoc:
-      config.generators.orm :mongo_odm, :migration => false
+      config.send( config.respond_to?(:app_generators) ? :app_generators : :generators ).
+        orm(:mongo_odm, :migration => false)
 
       rake_tasks do
         load 'mongo_odm/railties/database.rake'
