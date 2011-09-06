27a6d99b798e67045d90d864a2e4b47c622950b1
Steve Sloan <steve@finagle.org>
2011-09-06 13:37:18 -0700

    Delegate Criteria#each to cursor instead of #to_a.

--- lib/mongo_odm/criteria.rb
+++ lib/mongo_odm/criteria.rb
@@ -4,8 +4,8 @@ require 'mongo_odm/core_ext/hash_recursive_merge'
 module MongoODM
 
   class Criteria
-    delegate :to_xml, :to_yaml, :to_json, :include?, :first, :length, :collect, :map, :each, :all?, :include?, :to => :to_a
-    delegate :count, :to => :to_cursor
+    delegate :to_xml, :to_yaml, :to_json, :include?, :first, :length, :collect, :map, :all?, :include?, :to => :to_a
+    delegate :count, :each, :to => :to_cursor
 
     def initialize(klass, selector = {}, opts = {})
       @_klass    = klass
